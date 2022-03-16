b2fac2ab51dd8ee1b16ee82da141a12e01999381
mike <apolloproductionsdj@gmail.com>
2022-03-16 13:23:46 -0700

    added header

--- dj-events-fe/.history/pages/index_20220316130752.js
+++ dj-events-fe/.history/pages/index_20220316130752.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
 export default function HomePage() {
   return (
-    <Layout>
+    <div>
       <h1>Home Page</h1>
-    </Layout>
+    </div>
   );
 }
